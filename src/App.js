393b3fdfa834f7152a0d6e82a5da56d5652f6596
Galaxy159 <braun.asaf@gmail.com>
2022-11-18 08:11:30 +0200

    sfd

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
 
   return (
     <>
+      <h1></h1>
       <main className="container">
         <Card
           isSubmit={isSubmit}
--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -11,7 +11,7 @@ export default function Confirmation(props) {
           alt="icon-complete"
           className="confirmation__img"
         />
-        <h1 className="confirmation__headline">THANK YOU!</h1>
+        <p className="confirmation__headline">THANK YOU!</p>
         <p className="confirmation__text">We've added your card details</p>
         <button onClick={resetForm} className="confirmation__btn btn">
           Continue
