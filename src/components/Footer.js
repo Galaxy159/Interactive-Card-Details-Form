46c43daaaff958246ec987729dd80941eb184224
Galaxy159 <braun.asaf@gmail.com>
2022-11-26 17:10:55 +0200

    ds

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ export default function Footer() {
         </a>
         . Coded by{" "}
         <a
-          href="https://www.frontendmentor.io/profile/Galaxy159"
+          href="https://www.braunguitar.net"
           target="_blank"
-          rel="noreferrer"
+          rel="noreferrer noopener"
         >
-          Galaxy159
+          שיעורי גיטרה ברחובות
         </a>
         .
       </footer>
